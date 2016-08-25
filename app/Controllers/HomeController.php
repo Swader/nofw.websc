8694b19e0bc18e62544ade8ccce808829ddf127a
Bruno Skvorc <bruno@skvorc.me>
2016-08-25 22:31:15 +0200

    Added Monolog

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace nofw\app\Controllers;
 
+use Psr\Log\LoggerInterface;
+
 class HomeController
 {
     /** @var \Twig_Environment */
     private $twig;
 
-    public function __construct(\Twig_Environment $te)
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(\Twig_Environment $te, LoggerInterface $logger)
     {
         $this->twig = $te;
+        $this->logger = $logger;
     }
 
     public function indexAction()
     {
+        $this->logger->info('We are in index.');
         $this->twig->display('home/index.twig', ['message' => 'Yay!']);
     }
 }
--- public/index.php
+++ public/index.php
@@ -10,5 +10,18 @@ require_once '../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(ROOT.'/app/views');
 $te = new Twig_Environment($loader);
 
-$homeController = new HomeController($te);
+$logger = new \Monolog\Logger('nofwlog');
+
+$logger->pushHandler(
+    new Monolog\Handler\StreamHandler('../logs/all.log')
+);
+$logger->pushHandler(
+    new Monolog\Handler\StreamHandler('../logs/error.log', \Monolog\Logger::NOTICE)
+);
+
+Monolog\ErrorHandler::register($logger);
+
+$logger->info('Logging set up');
+
+$homeController = new HomeController($te, $logger);
 $homeController->indexAction();
